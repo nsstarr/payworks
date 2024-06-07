30bb0affb152d55c0e47a213c568f31ddbcda757
Anastasia Starostina <anastasia.st.stern@gmail.com>
2024-06-07 17:37:25 +0100

    feat: base nav set up

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -2,22 +2,30 @@ const Nav = () => {
   return (
     <nav
       id="navbar"
-      className="left-0 flex min-h-screen flex-col px-4 font-medium text-white-200"
+      className="left-0 flex min-h-screen flex-col px-4 font-medium text-white-200 bg-gradient-to-b from-deeppurple-600 to-deeppurple-500"
     >
       <a className="tracking-tigh mx-auto px-16 pb-9 pt-7 text-xl font-semibold">
-     <span>Payworks</span>
+        <h1 className="font-highlight font-semibold flex  bg-gray-100/10 px-4 py-2 rounded-3xl">
+          <div className="text-pink-500">Pay</div>Works
+        </h1>
       </a>
-      <ul className="gap-4">
-        <li className="cursor-pointer px-3 tracking-tight">
+      <ul className="flex flex-col gap-4">
+        <li className="cursor-pointer px-3 tracking-tight text-purple-200">
           <a>Dashboard</a>
         </li>
-        <li className="cursor-pointer px-3 py-1 tracking-tight">
-          <a>My Learning</a>
+        <li className="cursor-pointer px-3 py-1 tracking-tight text-purple-200">
+          <a>Helpdesk</a>
         </li>
-        <li className="cursor-pointer px-3 py-1 tracking-tight">
-          <a>Licenses</a>
+        <li className="cursor-pointer px-3 py-1 tracking-tight text-purple-200">
+          <a>Reports</a>
         </li>
-        <li className="cursor-pointer px-3 py-1 tracking-tight">
+        <li className="cursor-pointer px-3 py-1 tracking-tight text-purple-200">
+          <a>Statements</a>
+        </li>
+        <li className="cursor-pointer px-3 py-1 tracking-tight text-white-100 bg-purple-400 rounded-md">
+          <a>Payments</a>
+        </li>
+        <li className="cursor-pointer px-3 py-1 tracking-tight text-purple-200">
           <a>Settings</a>
         </li>
       </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -28,25 +28,26 @@ module.exports = {
         },
         purple: {
           100: "#F2F3F9",
+          200: "#7B809F",
           400: "#2D2D69",
           500: "#6750A4", // secondary button color
           700: "#353581", // gradienr color 1
           750: "#451D7B", // gradient color 2
           800: "#461985", // gradient color 3
         },
-        deep_purple: {
+        deeppurple: {
           500: "#2D0B5C", // nav gradienr color 1
           600: "#040440", // nav gradienr color 1
           700: "#484886", // filled in checkbox
         },
         pink: {
           100: "#EBE6F2",
+          500: "#EB40C7",
         },
         gray: {
-          100: "#F9FAFF",
+          100: "#F4F5F7",
           150: "#CAC4D0", // toast text color
-          200: "#9A9A9A",
-          300: "#A5A6AB",
+
         },
       },
     },
