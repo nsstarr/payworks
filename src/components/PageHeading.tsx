1c2cfa6e14601fd8e720d8e94c73e7611b146252
Anastasia Starostina <anastasia.st.stern@gmail.com>
2024-06-08 08:44:14 +0100

    fix:css edits to the PageHeading

--- src/components/PageHeading.tsx
+++ src/components/PageHeading.tsx
@@ -5,7 +5,7 @@ const PageHeading = () => {
       <span className="rounded-full bg-purple-150 px-3 py-2">
         <h1 className="font-highlight text-xl text-purple-400">42</h1>
       </span>
-      <h1 className="text-800 font-highlight font-semibold tracking-tight">
+      <h1 className="text-800 font-highlight text-xl font-semibold tracking-tight text-black-800">
         Invoices
       </h1>
     </div>
