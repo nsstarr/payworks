111e040a7afb58218e5b8d6ffd130e0ae0d5cfac
Anastasia Starostina <anastasia.st.stern@gmail.com>
2024-06-08 16:29:55 +0100

    feat: add shadow to the approve button

--- src/components/ApproveButton.tsx
+++ src/components/ApproveButton.tsx
@@ -2,7 +2,7 @@ import { Check } from "./glyphs/check";
 
 const ApproveButton = () => {
   return (
-    <button className="my-auto flex max-h-10 items-center gap-[5px] rounded-full bg-gradient-to-r from-purple-700 via-purple-750 to-purple-800 px-[18px] py-2 text-white-200 opacity-80">
+    <button className="my-auto flex max-h-10 items-center gap-[5px] rounded-full bg-gradient-to-r from-purple-700 via-purple-750 to-purple-800 px-[18px] py-2 text-white-200 opacity-80 shadow-md">
       <Check title="Check mark" />
       Approve All
     </button>
