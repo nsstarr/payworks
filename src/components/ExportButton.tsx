b6d57f948891749b37ea153f73c244bcc2ac2ea2
Anastasia Starostina <anastasia.st.stern@gmail.com>
2024-06-08 16:43:59 +0100

    feat: add export button text on hover

--- src/components/ExportButton.tsx
+++ src/components/ExportButton.tsx
@@ -1,9 +1,18 @@
+import { useState } from "react";
 import { ExportFile } from "./glyphs/export-file";
 
 const ExportCSV = () => {
+  const [isHovered, setIsHovered] = useState(false);
   return (
-    <button className="my-4 h-12 rounded-2xl border border-gray-200 bg-pink-100 px-3 shadow-md">
-      <ExportFile title="Export to CSV" className="p-0"/>
+    <button
+      className="hover:duration-600 my-4 flex h-12 items-center justify-center rounded-2xl border border-gray-200 bg-pink-100 px-3 shadow-md transition ease-in-out"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+    >
+      <ExportFile title="Export to CSV" className="p-0" />
+      <span className={`text-black-600 font-medium ${isHovered ? "pl-3" : ""}`}>
+        {isHovered ? "Export to CSV" : ""}
+      </span>
     </button>
   );
 };
