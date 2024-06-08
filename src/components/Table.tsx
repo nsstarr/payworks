c532a333d54979a5fb15a2e9ab25376853dbe6ce
Anastasia Starostina <anastasia.st.stern@gmail.com>
2024-06-08 16:29:15 +0100

    feat: add rounded corners to the table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -4,11 +4,11 @@ import { capitalizeFirstLetter } from "../utils/string-utils";
 
 const Table = () => {
   return (
-    <div className="w-full overflow-x-auto">
-      <table className="w-full table-auto border border-blue-200">
+    <div className="w-full overflow-x-auto rounded-2xl border border-blue-200">
+      <table className="w-full table-auto">
         <thead>
-          <tr className="ont-highlight rounded-2xl border border-blue-200 bg-white-200 text-left">
-            <th className="px-6 py-3 font-medium tracking-wider text-black-800">
+          <tr className="border-b border-blue-200 bg-white-200 text-left">
+            <th className="rounded-tl-2xl px-6 py-3 font-medium tracking-wider text-black-800">
               Invoice Number
             </th>
             <th className="px-6 py-3 font-medium tracking-wider text-black-800">
@@ -23,16 +23,20 @@ const Table = () => {
             <th className="px-6 py-3 font-medium tracking-wider text-black-800">
               Amount
             </th>
-            <th className="px-6 py-3 font-medium tracking-wider text-black-800">
+            <th className="rounded-tr-2xl px-6 py-3 font-medium tracking-wider text-black-800">
               Status
             </th>
           </tr>
         </thead>
         <tbody className="divide-y divide-gray-200">
-          {data.pay_run.invoices.map((invoice) => (
+          {data.pay_run.invoices.map((invoice, index) => (
             <tr
               key={invoice.supplier_reference}
-              className="text-gray-400"
+              className={`text-gray-400 ${
+                index === data.pay_run.invoices.length - 1
+                  ? "rounded-bl-2xl rounded-br-2xl"
+                  : ""
+              }`}
             >
               <td className="whitespace-nowrap px-6 py-4">
                 <div className="flex items-center gap-3">
