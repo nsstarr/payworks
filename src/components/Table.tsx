526dd642ed0591f6385a024b0d025c8cf6245e80
Anastasia Starostina <anastasia.st.stern@gmail.com>
2024-06-10 16:40:46 +0100

    feat: add infinite scroll

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -27,102 +27,100 @@ const Table = ({
 
   return (
     <div className="w-full overflow-x-auto rounded-2xl border border-blue-200">
-      <table className="w-full table-auto">
-        <thead>
-          <tr className="border-b border-blue-200 bg-white-200 text-left">
-            <th className="rounded-tl-2xl px-2 py-3 font-medium tracking-wider text-black-800">
-              <InvoiceSelect
-                selectedOption={selectedOption}
-                setSelectedOption={setSelectedOption}
-              />
-            </th>
-            <th className="px-6 py-3 font-medium tracking-wider text-black-800">
-              Invoice Number
-            </th>
-            <th className="px-6 py-3 font-medium tracking-wider text-black-800">
-              Supplier
-            </th>
-            <th className="px-6 py-3 font-medium tracking-wider text-black-800">
-              Amount
-            </th>
-            <th className="px-6 py-3 font-medium tracking-wider text-black-800">
-              Invoice Date
-            </th>
-            <th className="px-6 py-3 font-medium tracking-wider text-black-800">
-              Due Date
-            </th>
-            <th className="px-6 py-3 font-medium tracking-wider text-black-800">
-              Status
-            </th>
-            <th className="px-2 py-3 font-medium tracking-wider text-black-800 lg:visible lg:rounded-tr-2xl">
-              Actions
-            </th>
-          </tr>
-        </thead>
-        <tbody className="divide-y divide-gray-200">
-          {filteredInvoices.map((invoice, index) => (
-            <tr
-              key={invoice.supplier_reference}
-              className={`text-gray-400 ${
-                index === filteredInvoices.length - 1
-                  ? "rounded-bl-2xl rounded-br-2xl"
-                  : ""
-              }`}
-            >
-              <td className="whitespace-nowrap px-6 py-4">
-                <input
-                  type="checkbox"
-                  checked={excludedInvoices.includes(
-                    invoice.supplier_reference
-                  )}
-                  onChange={(e) =>
-                    handleCheckboxChange(
-                      invoice.supplier_reference,
-                      e.target.checked
-                    )
-                  }
+      <div className="table-container h-[calc(100vh-150px)] overflow-y-auto">
+        <table className="w-full table-auto">
+          <thead className="bg-white sticky top-0">
+            <tr className="border-b border-blue-200 bg-white-200 text-left">
+              <th className="rounded-tl-2xl px-2 py-3 font-medium tracking-wider text-black-800">
+                <InvoiceSelect
+                  selectedOption={selectedOption}
+                  setSelectedOption={setSelectedOption}
                 />
-              </td>
-              <td className="whitespace-nowrap px-6 py-4">
-                <div className="flex items-center gap-3">
-                  <div className="text-gray-400">{invoice.invoice_number}</div>
-                </div>
-              </td>
-              <td className="whitespace-nowrap px-6 py-4">
-                <div className="flex items-center gap-3">
-                  <div className="text-gray-400">{invoice.supplier}</div>
-                </div>
-              </td>
-              <td className="whitespace-nowrap px-6 py-4">
-                <div className="text-gray-400">
-                  {new Intl.NumberFormat("en-GB", {
-                    style: "currency",
-                    currency: "GBP",
-                  }).format(invoice.amount)}
-                </div>
-              </td>
-              <td className="whitespace-nowrap px-6 py-4">
-                <div className="text-gray-400">
-                  {formatDate(invoice.posted_date)}
-                </div>
-              </td>
-              <td className="whitespace-nowrap px-6 py-4">
-                <div className="text-gray-400">
-                  {formatDate(invoice.due_date)}
-                </div>
-              </td>
-              <td className="whitespace-nowrap px-6 py-4">
-                <div className="text-gray-400">
-                  {capitalizeFirstLetter(invoice.status)}
-                </div>
-              </td>
-              <td className="cursor-pointer whitespace-nowrap px-4 py-4 text-center">
-                <IconEdit title="Edit the invoice actions" />
-              </td>
+              </th>
+              <th className="px-6 py-3 font-medium tracking-wider text-black-800">
+                Invoice Number
+              </th>
+              <th className="px-6 py-3 font-medium tracking-wider text-black-800">
+                Supplier
+              </th>
+              <th className="px-6 py-3 font-medium tracking-wider text-black-800">
+                Amount
+              </th>
+              <th className="px-6 py-3 font-medium tracking-wider text-black-800">
+                Invoice Date
+              </th>
+              <th className="px-6 py-3 font-medium tracking-wider text-black-800">
+                Due Date
+              </th>
+              <th className="px-6 py-3 font-medium tracking-wider text-black-800">
+                Status
+              </th>
+              <th className="px-2 py-3 font-medium tracking-wider text-black-800 lg:visible lg:rounded-tr-2xl">
+                Actions
+              </th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody className="divide-y divide-gray-200">
+            {filteredInvoices.map((invoice, index) => (
+              <tr
+                key={invoice.supplier_reference}
+                className={`text-gray-400 ${
+                  index === filteredInvoices.length - 1
+                    ? "rounded-bl-2xl rounded-br-2xl"
+                    : ""
+                }`}
+              >
+                <td className="whitespace-nowrap px-6 py-4">
+                  <input
+                    type="checkbox"
+                    checked={excludedInvoices.includes(
+                      invoice.supplier_reference
+                    )}
+                    onChange={(e) =>
+                      handleCheckboxChange(
+                        invoice.supplier_reference,
+                        e.target.checked
+                      )
+                    }
+                  />
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  <div className="text-gray-400">{invoice.invoice_number}</div>
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  <div className="text-gray-400">{invoice.supplier}</div>
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  <div className="text-gray-400">
+                    {new Intl.NumberFormat("en-GB", {
+                      style: "currency",
+                      currency: "GBP",
+                    }).format(invoice.amount)}
+                  </div>
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  <div className="text-gray-400">
+                    {formatDate(invoice.posted_date)}
+                  </div>
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  <div className="text-gray-400">
+                    {formatDate(invoice.due_date)}
+                  </div>
+                </td>
+                <td className="whitespace-nowrap px-6 py-4">
+                  <div className="text-gray-400">
+                    {capitalizeFirstLetter(invoice.status)}
+                  </div>
+                </td>
+                <td className="whitespace-nowrap px-4 py-4 text-center">
+                  <IconEdit title="Edit the invoice actions" />
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 };
